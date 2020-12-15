cf9c3084fb1447ee55b5a55062f443dc0354b0e1
chialinkhern <chialinkhern@gmail.com>
2020-12-15 12:41:24 -0600

    added begin and end instructions

--- norm_images.php
+++ norm_images.php
@@ -4,6 +4,8 @@
     <script src="jspsych-6.0.1/jspsych.js"></script>
     <script src="jspsych-6.0.1/plugins/clk-norm-image.js"></script>
     <script src="jspsych-6.0.1/plugins/jspsych-survey-text.js"></script>
+    <script src="jspsych-6.0.1/plugins/jspsych-instructions.js"></script>
+    <script src="jspsych-6.0.1/plugins/jspsych-call-function.js"></script>
     <script src="helper_functions.js"></script>
     <link href="jspsych-6.0.1/css/jspsych.css" rel="stylesheet" type="text/css"></link>
 </head>
@@ -22,7 +24,7 @@
     let sub_num = Date.now()
     let out_data = {images: [], responses: [], rts: []}
 
-    show_image = {
+    let show_image = {
         type: "clk-norm-image",
         stimulus: "",
         prompt: "Type the name of this object. <p>Hit ENTER to proceed to next.</p>",
@@ -42,22 +44,38 @@
       loop_function: function(){
 	      iterate_images.img_num = iterate_images.img_num + 1
 	      console.log(out_data)
-	      if (iterate_images.img_num === 4){
+	      if (iterate_images.img_num === filenames.length){
 	          return false
 	      }
 	      else {return true}
       }
     }
 
-    timeline = [iterate_images]
-    jsPsych.init({
-        timeline: timeline,
-        show_preload_progress_bar: true,
-        on_finish: function(){
+    let instructions = {
+        type: "instructions",
+        pages: ["Name these objects!"],
+        show_clickable_nav: true
+    }
+
+    let end = {
+        type: "instructions",
+        pages: ["You're done. Thanks!"],
+        show_clickable_nav: false
+    }
+
+    let write_data = {
+        type: "call-function",
+        func: function(){
             let name = "subj_" + String(sub_num)
             let data = prep_data(out_data)
             save_data(name, data)
         }
+    }
+
+    timeline = [instructions, iterate_images, write_data, end]
+    jsPsych.init({
+        timeline: timeline,
+        show_preload_progress_bar: true,
     })
 
 </script>
