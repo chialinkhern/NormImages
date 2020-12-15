13594fc8ad78f22549ca22d09195e3bdf5c38f0b
chialinkhern <chialinkhern@gmail.com>
2020-12-15 11:50:30 -0600

    added Fisher-Yates Shuffle

--- norm_images.php
+++ norm_images.php
@@ -1,5 +1,3 @@
-<!-- TODO: you need to write save_data(); find new images for some; write randomize() -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,20 +16,39 @@
 ?>
 
 <script>
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
 
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+    // TODO: you need to write save_data(); find new images for some/find way to force size; write randomize()
 	let filenames = <?php echo json_encode($dirdata, JSON_HEX_TAG); ?>;
+	filenames = shuffle(filenames)
 
     // initialize empty data structure
+
     show_image = {
         type: "clk-norm-image",
         stimulus: "",
         prompt: "Type the name of this object. <p>Hit ENTER to proceed to next.</p>",
         on_start: function(){
             this.stimulus = "images/"+String(filenames[iterate_images.img_num])
-             //TODO autofocus!
         },
         on_finish: function(trialdata){
-        	// iterate_images.img_num = iterate_images.img_num + 1
             console.log(trialdata)
             // TODO needs to push data in readable format
         }
