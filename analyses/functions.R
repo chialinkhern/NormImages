d789dc84f342b060ed79a55b5bfa13516ecdfaf4
chialinkhern <chialinkhern@gmail.com>
2020-12-18 15:10:23 -0600

    added is_typo()

--- analyses/functions.R
+++ analyses/functions.R
@@ -3,22 +3,40 @@
 # Created by: chialinkhern
 # Created on: 12/18/20
 
-rbindall = function(indir, outdir){
-  originalwd = getwd()
-  setwd(indir)
-  files = list.files(pattern="*.csv")
-  trials = do.call(rbind, lapply(files, function(x) read_and_attach_name(x)))
-  trials$X = NULL # no idea why X appears as a column; this is an easy fix
-  trials$rts = as.numeric(trials$rts)
-  trials$responses = tolower(trials$responses)
-  setwd(originalwd)
-  write.csv(trials, outdir, row.names=FALSE)
+add_all_responses = function(items_df, trials_df){
+  for (image in items_df$images){
+    items_df$all_responses[items_df$images==image] = get_all_responses(image, trials_df)
+  }
+  return(items_df)
 }
 
-read_and_attach_name = function(path_to_file){ # because I was too shortsighted to include subnum in data files
-  df = read.csv(path_to_file, stringsAsFactors=FALSE)
-  df$subj_num = gsub(".csv", x=toString(path_to_file), replacement="")
-  return(df)
+add_proportion_correct = function(items_df){
+  for (item in items_df$images){
+    image_name = strsplit(item, "\\.")[[1]][1]
+    items_df$proportion_correct[items_df$images==item] = compute_proportion_correct(image_name, items_df[items_df$images==item,]$all_responses)
+  }
+  return (items_df)
+}
+
+
+add_top_3 = function(items_df){
+  for (item in items_df$images){
+    items_df$top_3[items_df$images==item] = get_top_3(items_df$all_responses[items_df$images==item])
+  }
+  return(items_df)
+}
+
+compute_proportion_correct = function(correct_response, all_responses){
+  all_responses = strsplit(all_responses, ",")[[1]]
+  num_responses = length(all_responses)
+  num_correct = 0
+  for (response in all_responses){
+    if ((response==correct_response)|is_typo(correct_response, response)){
+      num_correct = num_correct + 1
+    }
+  }
+  proportion_correct = num_correct/num_responses
+  return(proportion_correct)
 }
 
 get_all_responses = function(image, trials_df){
@@ -38,44 +56,48 @@ get_all_responses = function(image, trials_df){
   return(responses)
 }
 
-add_all_responses = function(items_df, trials_df){
-  for (image in items_df$images){
-    items_df$all_responses[items_df$images==image] = get_all_responses(image, trials_df)
-  }
-  return(items_df)
-}
-
 get_top_3 = function(responses){
   responses = strsplit(responses, ",")[[1]]
   responses = paste(names(sort(table(responses), decreasing=TRUE)[1:3]), collapse=",")
   return(responses)
 }
 
-add_top_3 = function(items_df){
-  for (item in items_df$images){
-    items_df$top_3[items_df$images==item] = get_top_3(items_df$all_responses[items_df$images==item])
-  }
-  return(items_df)
-}
-
-compute_proportion_correct = function(correct_response, all_responses){
-  all_responses = strsplit(all_responses, ",")[[1]]
-  num_responses = length(all_responses)
-  num_correct = 0
-  for (response in all_responses){
-    # TODO response = handle_typo(response)
-    if (response==correct_response){
-      num_correct = num_correct + 1
+is_typo = function(correct_response, response){ # returns whether or not response was a typo of correct_response
+  correct_response = strsplit(correct_response, "")[[1]]
+  response = strsplit(response, "")[[1]]
+  num_match = 0
+  for (i in correct_response){
+    j_index = 1
+    for (j in response){
+      if (i==j){
+        response[j_index] = "nope"
+        num_match = num_match + 1
+        break
+      }
+      j_index = j_index + 1
     }
   }
-  proportion_correct = num_correct/num_responses
-  return(proportion_correct)
+  proportion_match = num_match/length(correct_response)
+  if (proportion_match > 0.7 & (length(correct_response)>=length(response))){ # if 60% of correct_response's letters are found AND if response is not longer than correct_response
+    return(TRUE)
+  }
+  else {return(FALSE)}
 }
 
-add_proportion_correct = function(items_df){
-  for (item in items_df$images){
-    image_name = strsplit(item, "\\.")[[1]][1]
-    items_df$proportion_correct[items_df$images==item] = compute_proportion_correct(image_name, items_df[items_df$images==item,]$all_responses)
-  }
-  return (items_df)
+rbindall = function(indir, outdir){
+  originalwd = getwd()
+  setwd(indir)
+  files = list.files(pattern="*.csv")
+  trials = do.call(rbind, lapply(files, function(x) read_and_attach_name(x)))
+  trials$X = NULL # no idea why X appears as a column; this is an easy fix
+  trials$rts = as.numeric(trials$rts)
+  trials$responses = tolower(trials$responses)
+  setwd(originalwd)
+  write.csv(trials, outdir, row.names=FALSE)
+}
+
+read_and_attach_name = function(path_to_file){ # because I was too shortsighted to include subnum in data files
+  df = read.csv(path_to_file, stringsAsFactors=FALSE)
+  df$subj_num = gsub(".csv", x=toString(path_to_file), replacement="")
+  return(df)
 }
--- analyses/main.R
+++ analyses/main.R
@@ -7,10 +7,8 @@ trials_df = read.csv("analyses/out/data.csv")
 
 attach(trials_df)
 items_df = aggregate(rts~images, FUN="mean")
+detach(trials_df)
 colnames(items_df) = c("images", "mean_rts")
 items_df = add_all_responses(items_df, trials_df)
 items_df = add_top_3(items_df)
 items_df = add_proportion_correct(items_df)
-# D item_df$all_responses
-# item_df$top_5
-# item_df$proportion_correct
